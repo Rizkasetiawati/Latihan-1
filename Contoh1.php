39ba9d26193a38b19a63f3c857aee2017995e2a5
Rizka Setiawati <90634027+Rizkasetiawati@users.noreply.github.com>
2021-10-18 20:19:11 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rizka Setiawati
+             Saya tingga di daerah Perumnas 3 Bekasi Timur
+             olah raga yang saya sukai adalah
+             Bulutangkis";
+    }
+}
